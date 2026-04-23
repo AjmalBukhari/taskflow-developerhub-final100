47bbc9d8e5721b457b09ca37468e465471793d4c
AjmalBukhari <ajmalbukhari15@gmail.com>
2026-04-23 22:32:59 +0500

    update

--- backend/validators/taskValidator.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const taskSchema = Joi.object({
-  title: Joi.string().min(3).max(100).required(),
-  description: Joi.string().allow("", null),
-  status: Joi.string().valid("Pending", "In Progress", "Completed"),
-  dueDate: Joi.date().optional(),
-});
